dc6d8d7f220a7549fcbd6961c6c24c15613831f9
peiyi <peiyi523@gmail.com>
2023-11-16 23:49:26 +0800

    '更新views.py'

--- main/views.py
+++ main/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-import json
 from datetime import datetime
+import json
 import random
 from django.conf import settings
 from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseForbidden
@@ -10,17 +10,18 @@ from django.views.decorators.csrf import csrf_exempt
 from linebot import LineBotApi, WebhookHandler, WebhookParser
 from linebot.exceptions import InvalidSignatureError, LineBotApiError
 from linebot.models import MessageEvent, TextSendMessage, ImageSendMessage
-from invoice import get_invoice_numbers, search_invoice_bingo
+
+from crawel.invoice import get_invoice_numbers, search_invoice_bingo
 
 line_bot_api = LineBotApi(settings.LINE_CHANNEL_ACCESS_TOKEN)
 parse = WebhookParser(settings.LINE_CHANNEL_SECRET)
-
 start_invoice = False
+numbers = []
 
 
 @csrf_exempt
 def callback(request):
-    global start_invoice
+    global start_invoice, numbers
     if request.method == "POST":
         signature = request.META["HTTP_X_LINE_SIGNATURE"]
         body = request.body.decode("utf-8")
@@ -34,7 +35,7 @@ def callback(request):
             if isinstance(event, MessageEvent):
                 message = event.message.text
                 message_object = None
-                # 判斷是否進行對獎模式:
+                # 判斷是否進行對獎模式
                 if start_invoice:
                     if message == "0":
                         start_invoice = False
@@ -43,27 +44,24 @@ def callback(request):
                         message_text = search_invoice_bingo(message, numbers)
                         message_text += "\n==>請輸入下一組號碼(0:exit)"
                     message_object = TextSendMessage(text=message_text)
-
                 elif message == "發票":
                     numbers = get_invoice_numbers()
                     message_text = "進入發票對獎模式==>\n本期最新發票對獎號碼:" + ",".join(numbers)
                     message_text += "\n請開始輸入您的發票號碼(後三碼):"
                     message_object = TextSendMessage(text=message_text)
                     start_invoice = True
-
                 elif message == "你好":
-                    message_object = TextSendMessage(text="peiyi妳好!")
+                    message_object = TextSendMessage(text="jerry你好!")
                 elif "樂透" in message:
                     reply_message = "預測號碼為:\n" + get_lottory_number()
                     message_object = TextSendMessage(text=reply_message)
                 elif "捷運" in message:
                     if "台中" in message:
-                        image_url = "https://th.bing.com/th/id/OIP.qu5O4ncJrFeEV4I0rn60DQHaHa?pid=ImgDet&rs=1"
+                        image_url = "https://upload.wikimedia.org/wikipedia/commons/f/f4/%E5%8F%B0%E4%B8%AD%E6%8D%B7%E9%81%8B%E8%B7%AF%E7%B7%9A%E5%9C%96_%282020.01%29.png"
                     elif "高雄" in message:
-                        image_url = "https://upload.wikimedia.org/wikipedia/commons/3/33/%E9%AB%98%E9%9B%84%E6%8D%B7%E9%81%8B%E8%B7%AF%E7%B6%B2%E5%9C%96_(C1-C14%E7%AB%99%E5%90%8D%E7%A2%BA%E5%AE%9A%E7%89%88).png"
+                        image_url = "https://assets.piliapp.com/s3pxy/mrt_taiwan/kaohsiung/thumb_zh-Hant.png"
                     else:
-                        image_url = "https://assets.piliapp.com/s3pxy/mrt_taiwan/taipei/20190910_zh.png"
-
+                        image_url = "https://assets.piliapp.com/s3pxy/mrt_taiwan/taipei/20230214_zh.png"
                     message_object = ImageSendMessage(
                         original_content_url=image_url, preview_image_url=image_url
                     )
@@ -79,12 +77,12 @@ def callback(request):
         return HttpResponseBadRequest()
 
 
-# Create your views here.
 def index(request):
     now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     return HttpResponse(f"<h1>現在時刻:{now}</h1>")
 
 
+# Create your views here.
 def get_books(request):
     mybook = {1: "java-book", 2: "python-book", 3: "c-book"}
 
@@ -94,19 +92,20 @@ def get_books(request):
 def get_lottory2(request):
     numbers = sorted(random.sample(range(1, 50), 6))
     x = random.randint(1, 50)
-    numbers_str = " ".join(map(str, numbers))
-    return render(request, "lottory.html", {"numbers": numbers_str, "x": x})
+    number_str = " ".join(map(str, numbers))
+    return render(request, "lottory.html", {"numbers": number_str, "x": x})
 
 
 def get_lottory_number():
     numbers = sorted(random.sample(range(1, 50), 6))
     x = random.randint(1, 50)
-    numbers_str = " ".join(map(str, numbers)) + f" 特別號:{x}"
-    return numbers_str
+    number_str = " ".join(map(str, numbers)) + f" 特別號:{x}"
+    return number_str
 
 
 def get_lottory(request):
     numbers = sorted(random.sample(range(1, 50), 6))
+    print(numbers)
     x = random.randint(1, 50)
-    numbers_str = " ".join(map(str, numbers)) + f" 特別號:{x}"
-    return HttpResponse(f"<h1>本期預測號碼:</h1>" + "<h2>" + numbers_str + "</h2>")
+    number_str = " ".join(map(str, numbers)) + f" 特別號:{x}"
+    return HttpResponse("<h1>本期預測號碼:</h1>" + "<h2>" + number_str + "</h2>")
